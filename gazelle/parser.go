52cd4c4291b8f2b535f18a51047346ea203836ec
Alex Eagle <alex@aspect.dev>
2022-03-04 10:54:17 -0800

    add parser

--- gazelle/parser.go
+++ gazelle/parser.go
@@ -16,7 +16,89 @@ import (
 	"github.com/bazelbuild/rules_go/go/tools/bazel"
 	"github.com/emirpasic/gods/sets/treeset"
 	godsutils "github.com/emirpasic/gods/utils"
+	"path/filepath"
+
+	"github.com/evanw/esbuild/pkg/api"
 )
+
+type Parser struct {
+}
+
+func NewParser() *Parser {
+	p := &Parser{}
+	return p
+}
+
+// filenameToLoader takes in a filename, e.g. myFile.ts,
+// and returns the appropriate esbuild loader for that file.
+func filenameToLoader(filename string) api.Loader {
+	ext := filepath.Ext(filename)
+	switch ext {
+	case ".ts":
+		return api.LoaderTS
+	case ".tsx":
+		return api.LoaderTSX
+	case ".js":
+		return api.LoaderJSX
+	case ".jsx":
+		return api.LoaderJSX
+	default:
+		return api.LoaderTS
+	}
+}
+
+// ParseImports returns all the imports from a file
+// after parsing it.
+func (p *Parser) ParseImports(fileInfo *FileInfo) []string {
+	imports := []string{}
+	if filepath.Ext(fileInfo.Filename) == ".css" {
+		// No need to try to parse CSS.
+		return imports
+	}
+	// Construct an esbuild plugin that pulls out all the imports.
+	plugin := api.Plugin{
+		Name: "GetImports",
+		Setup: func(pluginBuild api.PluginBuild) {
+			// callback is a handler for esbuild resolutions. This is how
+			// we'll get access to every import in the file.
+			callback := func(args api.OnResolveArgs) (api.OnResolveResult, error) {
+				// Add the imported string to our list of imports.
+				imports = append(imports, args.Path)
+				return api.OnResolveResult{
+					// Mark the import as external so esbuild doesn't complain
+					// about not being able to find the import.
+					External: true,
+				}, nil
+			}
+
+			// pluginBuild.OnResolve sets the plugin's onResolve callback to our custom callback.
+			// Make sure to set Filter: ".*" so that our plugin runs on all imports.
+			pluginBuild.OnResolve(api.OnResolveOptions{Filter: ".*", Namespace: ""}, callback)
+		},
+	}
+	options := api.BuildOptions{
+		Stdin: &api.StdinOptions{
+			Contents:   fileInfo.Content,
+			Sourcefile: fileInfo.Filename,
+			// The Loader determines how esbuild will parse the file.
+			// We want to parse .ts files as typescript, .tsx files as .tsx, etc.
+			Loader: filenameToLoader(fileInfo.Filename),
+		},
+		Plugins: []api.Plugin{
+			plugin,
+		},
+		// Must set bundle to true so that esbuild actually does resolutions.
+		Bundle: true,
+	}
+	result := api.Build(options)
+	if len(result.Errors) > 0 {
+		// Inform users that some files couldn't be fully parsed.
+		// No need to crash the program though.
+		log.Printf("Encountered errors parsing source %v: %v\n", fileInfo.Filename, result.Errors)
+	}
+
+	return imports
+}
 
 var (
 	parserStdin  io.Writer
